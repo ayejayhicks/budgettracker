cc21651fe114e0a8f48adbe209bae5b4ad0fe242
Alaina Hicks <ajhicks0816@gmail.com>
2021-04-29 21:14:17 -0500

    bla

--- Develop/server.js
+++ Develop/server.js
@@ -15,13 +15,13 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb+srv://ajhicks0816:CannonGrae0816@cluster0.vlxo4.mongodb.net/budgettracker?retryWrites=true&w=majority", {
+mongoose.connect("mongodb+srv://ajhicks0816:CannonGrae0816@cluster0.vlxo4.mongodb.net/budgettracker", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
   useFindAndModify: false
 });
-
+console.log('this is the most updated version')
 // routes
 app.use(require("./routes/api.js"));
 
